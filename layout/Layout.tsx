6872b4d640cfdc24e46d4c4b6aa3f0a361ff9a79
Andrey Emelianov <621077@mail.ru>
2023-06-14 20:56:34 +0300

    реализовал простой hoc для оборачивания компонентов лаяутом

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,9 +1,10 @@
+import { FunctionComponent } from 'react';
 import { Footer } from './Footer/Footer';
 import { Header } from './Header/Header';
 import { ILayoutProps } from './Layout.props';
 import { Sidebar } from './Sidebar/Sidebar';
 
-export const Layout = ({ children }: ILayoutProps): JSX.Element => {
+const Layout = ({ children }: ILayoutProps): JSX.Element => {
 	return (
 		<>
 			<Header />
@@ -14,4 +15,16 @@ export const Layout = ({ children }: ILayoutProps): JSX.Element => {
 			<Footer />
 		</>
 	);
+};
+
+export const withLayout = <T extends Record<string, unknown>>(
+	Component: FunctionComponent<T>
+) => {
+	return function withLayoutComponent(props: T): JSX.Element {
+		return (
+			<Layout>
+				<Component {...props} />
+			</Layout>
+		);
+	};
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-import { Button, Htag, P, Rating, Tag } from '@/components';
-import { Layout } from '@/layout/Layout';
 import { useState } from 'react';
+import { Button, Htag, P, Rating, Tag } from '@/components';
+import { withLayout } from '@/layout/Layout';
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
 	const [rating, setRating] = useState<number>(4);
 
 	return (
-		<Layout>
+		<>
 			<Htag tag="h1">Text</Htag>
 			<Button appearance="primary" arrow="right">
 				Btne
@@ -26,6 +26,8 @@ export default function Home(): JSX.Element {
 			</Tag>
 			<Tag color="primary">primary</Tag>
 			<Rating rating={rating} isEditable setRating={setRating} />
-		</Layout>
+		</>
 	);
 }
+
+export default withLayout(Home);
