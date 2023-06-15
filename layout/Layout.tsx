f1cba018f95097189dfba88a8ce6fc3417e38e57
Andrey Emelianov <621077@mail.ru>
2023-06-15 20:11:13 +0300

    создал контекст и обернул им весь лаяут

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent } from 'react';
 import cn from 'classnames';
 
+import { AppContextProvider, IAppContext } from '@/context/app.context';
 import { Footer } from './Footer/Footer';
 import { Header } from './Header/Header';
 import { Sidebar } from './Sidebar/Sidebar';
@@ -19,14 +20,16 @@ const Layout = ({ children }: ILayoutProps): JSX.Element => {
 	);
 };
 
-export const withLayout = <T extends Record<string, unknown>>(
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(
 	Component: FunctionComponent<T>
 ) => {
 	return function withLayoutComponent(props: T): JSX.Element {
 		return (
-			<Layout>
-				<Component {...props} />
-			</Layout>
+			<AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
+				<Layout>
+					<Component {...props} />
+				</Layout>
+			</AppContextProvider>
 		);
 	};
 };
