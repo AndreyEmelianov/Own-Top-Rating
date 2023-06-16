4f76683402b84846b49c9870238bd99538f5677f
Andrey Emelianov <621077@mail.ru>
2023-06-16 12:33:39 +0300

    реализовал обработчик для меню второй категории которое скрывает или показывает его содержимое при клике и добавил стили

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import cn from 'classnames';
 import { format } from 'date-fns';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import { AppContext } from '@/context/app.context';
 import { IFirstLevelMenuItem, IPageItem } from '@/interfaces/menu.interface';
@@ -43,6 +44,19 @@ const firstLevelMenu: IFirstLevelMenuItem[] = [
 
 export const Menu = (): JSX.Element => {
 	const { menu, setMenu, firstCategory } = useContext(AppContext);
+	const router = useRouter();
+
+	const openSecondLevelMenuHandler = (secondCategory: string) => {
+		setMenu &&
+			setMenu(
+				menu.map((menuItem) => {
+					if (menuItem._id.secondCategory == secondCategory) {
+						menuItem.isOpened = !menuItem.isOpened;
+					}
+					return menuItem;
+				})
+			);
+	};
 
 	const buildFirstLevel = () => {
 		return (
@@ -69,18 +83,35 @@ export const Menu = (): JSX.Element => {
 	const buildSecondLevel = (menuItem: IFirstLevelMenuItem) => {
 		return (
 			<div className={styles.secondBlock}>
-				{menu.map((menuCategory) => (
-					<div key={menuCategory._id.secondCategory}>
-						<div className={styles.secondLevel}>{menuCategory._id.secondCategory}</div>
-						<div
-							className={cn(styles.secondLevelBlock, {
-								[styles.secondLevelBlockOpened]: menuCategory.isOpened,
-							})}
-						>
-							{buildThirdLevel(menuCategory.pages, menuItem.route)}
+				{menu.map((menuCategory) => {
+					if (
+						menuCategory.pages
+							.map((page) => page.alias)
+							.includes(router.asPath.split('/')[2])
+					) {
+						menuCategory.isOpened = true;
+					}
+
+					return (
+						<div key={menuCategory._id.secondCategory}>
+							<div
+								className={styles.secondLevel}
+								onClick={() =>
+									openSecondLevelMenuHandler(menuCategory._id.secondCategory)
+								}
+							>
+								{menuCategory._id.secondCategory}
+							</div>
+							<div
+								className={cn(styles.secondLevelBlock, {
+									[styles.secondLevelBlockOpened]: menuCategory.isOpened,
+								})}
+							>
+								{buildThirdLevel(menuCategory.pages, menuItem.route)}
+							</div>
 						</div>
-					</div>
-				))}
+					);
+				})}
 			</div>
 		);
 	};
@@ -91,7 +122,7 @@ export const Menu = (): JSX.Element => {
 				href={`/${route}/${page.alias}`}
 				key={page._id}
 				className={cn(styles.thirdLevel, {
-					[styles.thirdLevelActive]: false,
+					[styles.thirdLevelActive]: `/${route}/${page.alias}` == router.asPath,
 				})}
 			>
 				{page.category}
