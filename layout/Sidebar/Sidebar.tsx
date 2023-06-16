d52c1782b9981de4efd34f508eab3bad54578fd2
Andrey Emelianov <621077@mail.ru>
2023-06-16 14:51:20 +0300

    добавил лого и базовые стили для сайдбара

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,12 +1,16 @@
 import { ISidebarProps } from './Sidebar.props';
+import cn from 'classnames';
 
 import { Menu } from '../Menu/Menu';
 
+import LogoIcon from '../logo.svg';
 import styles from './Sidebar.module.css';
 
-export const Sidebar = ({ ...props }: ISidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props }: ISidebarProps): JSX.Element => {
 	return (
-		<div {...props}>
+		<div className={cn(className, styles.sidebar)} {...props}>
+			<LogoIcon className={styles.logo} />
+			<div>search</div>
 			<Menu />
 		</div>
 	);
