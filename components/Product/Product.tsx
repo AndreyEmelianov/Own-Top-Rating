b9eacbfce0348dfa4e9aea02392d66864cebea09
Andrey Emelianov <621077@mail.ru>
2023-06-16 21:01:10 +0300

    наполнил компонент Product содержимым и приступил к стилизации

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,9 +1,34 @@
 import cn from 'classnames';
 
 import { IProductProps } from './Product.props';
+import { Card } from '../Card/Card';
+import { Rating } from '../Rating/Rating';
+import { Tag } from '../Tag/Tag';
 
 import styles from './Product.module.css';
 
 export const Product = ({ product, className, ...props }: IProductProps): JSX.Element => {
-	return <div>{product.title}</div>;
+	return (
+		<Card className={styles.product}>
+			<div className={styles.logo}>
+				<img src={product.image} alt={product.title} />
+			</div>
+			<div className={styles.title}>{product.title}</div>
+			<div className={styles.price}>{product.price}</div>
+			<div className={styles.credit}>{product.credit}</div>
+			<div className={styles.rating}>
+				<Rating rating={product.reviewAvg ?? product.initialRating} />
+			</div>
+			<div className={styles.tags}>
+				{product.categories.map((category) => (
+					<Tag color="ghost" key={category}>
+						{category}
+					</Tag>
+				))}
+			</div>
+			<div className={styles.priceTitle}>цена</div>
+			<div className={styles.creditTitle}>кредит</div>
+			<div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+		</Card>
+	);
 };
