02656fd49260c2d2f74fb2746c9ae75624eda124
Andrey Emelianov <621077@mail.ru>
2023-06-16 14:04:51 +0300

    перенёс иконки и данные о меню первой категории в отдельный компонент в папку helpers

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,46 +1,13 @@
 import { useContext } from 'react';
 import cn from 'classnames';
-import { format } from 'date-fns';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
+import { firstLevelMenu } from '@/helpers/helpers';
 import { AppContext } from '@/context/app.context';
 import { IFirstLevelMenuItem, IPageItem } from '@/interfaces/menu.interface';
-import { TopLevelCategory } from '@/interfaces/toppage.interface';
-
-import CoursesIcon from './icons/courses.svg';
-import ServicesIcon from './icons/services.svg';
-import BooksIcon from './icons/books.svg';
-import ProductsIcon from './icons/product.svg';
 
 import styles from './Menu.module.css';
-
-const firstLevelMenu: IFirstLevelMenuItem[] = [
-	{
-		route: 'courses',
-		name: 'Курсы',
-		icon: <CoursesIcon />,
-		id: TopLevelCategory.Courses,
-	},
-	{
-		route: 'services',
-		name: 'Сервисы',
-		icon: <ServicesIcon />,
-		id: TopLevelCategory.Services,
-	},
-	{
-		route: 'books',
-		name: 'Книги',
-		icon: <BooksIcon />,
-		id: TopLevelCategory.Books,
-	},
-	{
-		route: 'products',
-		name: 'Продукты',
-		icon: <ProductsIcon />,
-		id: TopLevelCategory.Products,
-	},
-];
 
 export const Menu = (): JSX.Element => {
 	const { menu, setMenu, firstCategory } = useContext(AppContext);
