faa3f5663ac87237d892f6d21f7a3caecba44c64
Andrey Emelianov <621077@mail.ru>
2023-06-16 17:10:45 +0300

    добавил функцию с регулярными выражениями для более удобного вывода сумм зарплат

--- components/HhData/HhData.tsx
+++ components/HhData/HhData.tsx
@@ -1,5 +1,6 @@
 import cn from 'classnames';
 
+import { priceRu } from '@/helpers/helpers';
 import { Card } from '../Card/Card';
 import { IHhDataProps } from './HhData.props';
 
@@ -26,7 +27,7 @@ export const HhData = ({
 				{/* первый блок */}
 				<div>
 					<div className={styles.title}>Начальный</div>
-					<div className={styles.salaryValue}>{juniorSalary}</div>
+					<div className={styles.salaryValue}>{priceRu(juniorSalary)}</div>
 					<div className={styles.rate}>
 						<RateIcon className={styles.filled} />
 						<RateIcon />
@@ -36,7 +37,7 @@ export const HhData = ({
 				{/* второй блок */}
 				<div>
 					<div className={styles.title}>Средний</div>
-					<div className={styles.salaryValue}>{middleSalary}</div>
+					<div className={styles.salaryValue}>{priceRu(middleSalary)}</div>
 					<div className={styles.rate}>
 						<RateIcon className={styles.filled} />
 						<RateIcon className={styles.filled} />
@@ -46,7 +47,7 @@ export const HhData = ({
 				{/* третий блок */}
 				<div>
 					<div className={styles.title}>Профессионал</div>
-					<div className={styles.salaryValue}>{seniorSalary}</div>
+					<div className={styles.salaryValue}>{priceRu(seniorSalary)}</div>
 					<div className={styles.rate}>
 						<RateIcon className={styles.filled} />
 						<RateIcon className={styles.filled} />
