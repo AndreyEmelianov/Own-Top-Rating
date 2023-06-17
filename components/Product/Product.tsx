6b34132a780c290bf07030e5eaa3eca12248bc09
Andrey Emelianov <621077@mail.ru>
2023-06-17 13:25:00 +0300

    добавил в helpers функцию склонения слова в зависимости от кол-ва отзывов

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -4,7 +4,7 @@ import { IProductProps } from './Product.props';
 import { Card } from '../Card/Card';
 import { Rating } from '../Rating/Rating';
 import { Tag } from '../Tag/Tag';
-import { priceRu } from '@/helpers/helpers';
+import { declOfNum, priceRu } from '@/helpers/helpers';
 import { Button } from '../Button/Button';
 import { Divider } from '../Divider/Divider';
 
@@ -41,7 +41,10 @@ export const Product = ({ product, className, ...props }: IProductProps): JSX.El
 			</div>
 			<div className={styles.priceTitle}>цена</div>
 			<div className={styles.creditTitle}>кредит</div>
-			<div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+			<div className={styles.rateTitle}>
+				{product.reviewCount}{' '}
+				{declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}
+			</div>
 			<Divider className={styles.hr} />
 
 			{/* описание продукта */}
