c41b9dc68e7cbbc5cdbbdff39ae49f20abfeb18e
Andrey Emelianov <621077@mail.ru>
2023-06-17 13:46:11 +0300

    использова встроенный в next компонент Image для картинок курсов

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,4 +1,5 @@
 import cn from 'classnames';
+import Image from 'next/image';
 
 import { IProductProps } from './Product.props';
 import { Card } from '../Card/Card';
@@ -15,7 +16,12 @@ export const Product = ({ product, className, ...props }: IProductProps): JSX.El
 		// head продукта
 		<Card className={styles.product}>
 			<div className={styles.logo}>
-				<img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} />
+				<Image
+					src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
+					alt={product.title}
+					width={70}
+					height={70}
+				/>
 			</div>
 			<div className={styles.title}>{product.title}</div>
 			<div className={styles.price}>
